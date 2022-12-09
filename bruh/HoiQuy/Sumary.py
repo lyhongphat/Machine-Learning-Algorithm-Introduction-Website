b9f028d136ca96085c897c41b33b7dfccf9b7f65
Khowfsix <89312102+Khowfsix@users.noreply.github.com>
2022-12-09 15:26:16 +0700

    deploy

--- bruh/HoiQuy/Sumary.py
+++ bruh/HoiQuy/Sumary.py
@@ -7,25 +7,25 @@ def display():
     tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(
         ["Các khái niệm", "Ví dụ 1", "Ví dụ 2", "Ví dụ 3", "Ví dụ 4", "Ví dụ 5"])
     with tab1:
-        import HoiQuy.Theory
-        HoiQuy.Theory.display()
+        import bruh.HoiQuy.Theory as theory
+        theory.display()
     with tab2:
         st.title("Ví dụ hồi quy tuyến tính")
-        import HoiQuy.Bai01
-        HoiQuy.Bai01.display()
+        import bruh.HoiQuy.Bai01
+        bruh.HoiQuy.Bai01.display()
     with tab3:
         st.title("Ví dụ đường hồi quy tuyến tính")
-        import HoiQuy.Bai02
-        HoiQuy.Bai02.display()
+        import bruh.HoiQuy.Bai02
+        bruh.HoiQuy.Bai02.display()
     with tab4:
         st.title("Ví dụ minh họa")
-        import HoiQuy.Bai03
-        HoiQuy.Bai03.display()
+        import bruh.HoiQuy.Bai03
+        bruh.HoiQuy.Bai03.display()
     with tab5:
         st.title("Ví dụ minh họa")
-        import HoiQuy.Bai04
-        HoiQuy.Bai04.display()
+        import bruh.HoiQuy.Bai04
+        bruh.HoiQuy.Bai04.display()
     with tab6:
         st.title("Ví dụ minh họa")
-        import HoiQuy.Bai05
-        HoiQuy.Bai05.display()
+        import bruh.HoiQuy.Bai05
+        bruh.HoiQuy.Bai05.display()
--- bruh/Overfitting/Sumary.py
+++ bruh/Overfitting/Sumary.py
@@ -5,24 +5,24 @@ def display():
     st.title("Overfitting")
     tab1, tab2, tab3 = st.tabs(["Overfitting là gì", "Ví dụ", "Cách tránh Overfitting"])
     with tab1:
-        import Overfitting.Theory
-        Overfitting.Theory.display()
+        import bruh.Overfitting.Theory
+        bruh.Overfitting.Theory.display()
     with tab2:
         st.title("Ví dụ")
         option = st.selectbox("Chọn bậc của đa thức", [2, 4, 8, 16])
         if option == 2:
-            import Overfitting.Bai01a
-            Overfitting.Bai01a.display()
+            import bruh.Overfitting.Bai01a
+            bruh.Overfitting.Bai01a.display()
         elif option == 4:
-            import Overfitting.Bai01b
-            Overfitting.Bai01b.display()
+            import bruh.Overfitting.Bai01b
+            bruh.Overfitting.Bai01b.display()
         elif option == 8:
-            import Overfitting.Bai01c
-            Overfitting.Bai01c.display()
+            import bruh.Overfitting.Bai01c
+            bruh.Overfitting.Bai01c.display()
         elif option == 16:
-            import Overfitting.Bai01d
-            Overfitting.Bai01d.display()
+            import bruh.Overfitting.Bai01d
+            bruh.Overfitting.Bai01d.display()
 
     with tab3:
-        import Overfitting.avoidOverfitting as avOf
+        import bruh.Overfitting.avoidOverfitting as avOf
         avOf.display()
