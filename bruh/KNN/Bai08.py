573892215cc24e5ca94bb148e0918d715f965960
VoVanDuc20110635 <20110635@student.hcmute.edu.vn>
2022-12-12 00:32:54 +0700

    cap nhat KNN

--- bruh/KNN/Bai08.py
+++ bruh/KNN/Bai08.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from PIL import ImageTk, Image
-
+from PIL import Image
+import os
+import streamlit as st
 
 class App(tk.Tk):
     def __init__(self):
@@ -22,8 +24,50 @@ class App(tk.Tk):
         self.image_tk = ImageTk.PhotoImage(img)
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.image_tk)
 
+def executeThisFunction():
+    currentDir = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(currentDir, "../assets/images/KNN/castle.jpg")
+    image = Image.open(filepath)
+    st.image(image, caption='', use_column_width=True)
+
+    code = '''def __init__(self):
+        columnconfigure(0, weight=1)
+        rowconfigure(0, weight=1)
+        resizable(True, True)
+        image = Image.open('castle.jpg')
+        canvas = tk.Canvas(self, relief=tk.SUNKEN, borderwidth=0, bg='white', highlightthickness=0)
+        canvas.grid(row=0, column=0, sticky=tk.NSEW, padx=5, pady=5)
+        canvas.bind("<Configure>", self.configure)'''
+    st.code(code, language="python")
+    st.write("Phương thức columnconfigure() định cấu hình chỉ mục cột của lưới.")
+    st.write("Phương thức columnconfigure() định cấu hình chỉ mục hàng của lưới.")
+    st.write("Phương thức resizable() được sử dụng để cho phép cửa sổ gốc thay đổi kích thước của nó theo nhu cầu của người dùng")
+    st.write("Mở ảnh castle.jpg bằng Image.open")
+    st.write("Tiên hành mở ảnh với không đậm đường liền, không highlight")
+    st.write("Phương thức grid() cho phép bạn chỉ định vị trí hàng và cột trong danh sách tham số của nó.")
+
+
+    code = '''def configure(self, event):
+        canvas.delete('all')
+        canvas.update()
+        W = self.canvas.winfo_width()
+        H = self.canvas.winfo_height()
+        img = self.image.resize((W, H), Image.ANTIALIAS)
+        image_tk = ImageTk.PhotoImage(img)
+        canvas.create_image(0, 0, anchor=tk.NW, image=self.image_tk)'''
+    st.code(code, language = "python")
+    st.write("canvas.delete dùng để loại bỏ những dữ liệu chứa trong bộ nhớ tạm của nó")
+    st.write("canvas.update tiến hành cập nhật lại những thông tin mới được cung cấp")
+    st.write("Tiến hành thiết lập độ dài, rộng cho khung tranh")
+    st.write("tiến hành sử dụng phương thức resize() để đặt lại kích thước ảnh")
+
+
+
+
+
+    # image = Image.open('castle.jpg')
+    # st.image(image, caption='Output', use_column_width=True)
 
 if __name__ == "__main__":
-    app = App()
-    app.title('Bai Thuc Hanh So 8')
-    app.mainloop()
+    executeThisFunction()
+
--- bruh/KNN/Sumary.py
+++ bruh/KNN/Sumary.py
@@ -2,16 +2,39 @@ import streamlit as st
 
 
 def display():
-    tab0, tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs([
+    st.title("""Ví dụ minh họa""")
+    st.header("""K-nearest neighbor""")
+    tab0, tab1, tab2, tab3, tab6 = st.tabs([
         "Lý thuyết",
         "Ví dụ 1",
         "Ví dụ 2",
         "Ví dụ 3",
         "Ví dụ 4",
-        "Ví dụ 5",
-        "Ví dụ 6",
+
     ])
 
     with tab0:
         import bruh.KNN.Theory as theory
         theory.display()
+    with tab1:
+        import bruh.KNN.Bai01 as Bai01
+        Bai01.executeThisFunction()
+    with tab2:
+        import bruh.KNN.Bai02 as Bai02
+        Bai02.executeThisFunction()
+
+    with tab3:
+        import bruh.KNN.Bai03 as Bai03
+        Bai03.executeThisFunction()
+        # st.write("")
+    # with tab4:
+    #     import bruh.KNN.Bai03a as Bai03a
+    #     Bai03a.executeThisFunction()
+    #     # st.write("")
+    # with tab5:
+    #     import bruh.KNN.Bai04 as Bai04
+    #     Bai04.executeThisFunction()
+    #     # st.write("")
+    with tab6:
+        import  bruh.KNN.Bai08 as Bai08
+        Bai08.executeThisFunction()
